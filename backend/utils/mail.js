ac5c21e8b22835806d0ca8a8bd9df66475412991
sambit sourav sahoo <sambitsouravsahoo14@gmail.com>
2025-12-20 01:25:05 +0530

    first commit

---
+++ backend/utils/mail.js
@@ -0,0 +1,34 @@
+import nodemailer from "nodemailer"
+import dotenv from "dotenv"
+dotenv.config()
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,              // ✅ CHANGE
+  secure: false,  // true for 465, false for other ports
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASS,
+  },
+  tls: {
+    rejectUnauthorized: false // ✅ FIX SSL ERROR
+  }
+});
+
+export const sendOtpMail=async (to,otp) => {
+    await transporter.sendMail({
+        from:process.env.EMAIL,
+        to,
+        subject:"Reset Your Password",
+        html:`<p>Your OTP for password reset is <b>${otp}</b>. It expires in 5 minutes.</p>`
+    })
+}
+
+
+export const sendDeliveryOtpMail=async (user,otp) => {
+    await transporter.sendMail({
+        from:process.env.EMAIL,
+        to:user.email,
+        subject:"Delivery OTP",
+        html:`<p>Your OTP for delivery is <b>${otp}</b>. It expires in 5 minutes.</p>`
+    })
+}
